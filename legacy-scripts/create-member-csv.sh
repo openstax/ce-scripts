d5d8d7c36a238f284f2e72b4a6e8451385886418
therealmarv <1050582+therealmarv@users.noreply.github.com>
2020-06-24 11:56:56 +0300

    wip

--- legacy-scripts/create-member-csv.sh
+++ legacy-scripts/create-member-csv.sh
@@ -1,19 +1,48 @@
 #!/bin/bash
+set -euo pipefail
+countfile=membercount.txt
+csvfile=members.csv
+
 function get_members() {
-    echo start $1 end $2
+    echo get members from $1 to $2
+    # TODO: run command
+    echo /var/lib/cnx/cnx-buildout/bin/instance run member2csv.py $1 $2 $csvfile
 }
-membercount=20001
+
+function backup_file() {
+    if [ -f "$1" ]; then
+        # TODO: uncomment
+        # mv -f "$1" "$1.bak"
+        echo renaming file here # TODO:
+    fi
+}
+
+# delete/backup old files
+backup_file $countfile
+backup_file $csvfile
+
+# get member count
+echo get member count
+# TODO: run command
+echo /var/lib/cnx/cnx-buildout/bin/instance run getmembercount.py $countfile
+
+if [ -f "$countfile" ]; then
+    membercount=$(head -n 1 $countfile)
+else
+    echo "$countfile missing (total number of legacy members)"
+    exit 1
+fi
 stepcount=10000
 steps=$((membercount / stepcount))
 remainder=$((membercount % stepcount))
 
-echo $steps
-echo $remainder
-for step in $(seq 0 $((steps - 1))); do
-    start=$((step * stepcount))
-    end=$((start + stepcount - 1))
-    get_members $start $end
-done
+if [ $steps -gt 0 ]; then # never do negative stepping
+    for step in $(seq 0 $((steps - 1))); do
+        start=$((step * stepcount))
+        end=$((start + stepcount - 1))
+        get_members $start $end
+    done
+fi
 if [ $remainder -gt 0 ]; then
     start=$((steps * stepcount))
     end=$((start + remainder - 1))
