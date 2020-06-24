f8d223dec0ccdc4e37f29942b4defba82266839a
therealmarv <1050582+therealmarv@users.noreply.github.com>
2020-06-24 19:50:29 +0200

    pep-8

--- legacy-scripts/member2csv.py
+++ legacy-scripts/member2csv.py
@@ -5,6 +5,7 @@ import csv
 from Products.CMFCore.utils import getToolByName
 from Products.PluggableAuthService.interfaces.plugins import IRoleAssignerPlugin
 
+
 def get_member_data(start, end):
 
     def get_member_info(member_id):
@@ -30,27 +31,31 @@ def get_member_data(start, end):
             formatted_member_roles = " ".join(member_roles)
             yield [member_id_name, member_email, member_firstname, member_lastname, member_fullname, formatted_member_roles]
 
+
 def main():
     csv_filename = sys.argv[1]
     start_index = int(sys.argv[2])
     end_index = int(sys.argv[3])
 
     if start_index > 0:
-        writemode='a'
+        writemode = 'a'
     else:
-        writemode='w'
+        writemode = 'w'
 
     csv_file = open(csv_filename, mode=writemode)
-    writer = csv.writer(csv_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+    writer = csv.writer(csv_file, delimiter=',',
+                        quotechar='"', quoting=csv.QUOTE_MINIMAL)
     if start_index == 0:
         # write header in old python 2.4 way
-        writer.writerow(['id', 'email', 'first_name', 'last_name', 'full_name', 'roles'])
+        writer.writerow(['id', 'email', 'first_name',
+                         'last_name', 'full_name', 'roles'])
 
     for member in get_member_data(start_index, end_index):
         writer.writerow(member)
 
     csv_file.flush()
     csv_file.close()
+
 
 if __name__ == "__main__":
     main()
